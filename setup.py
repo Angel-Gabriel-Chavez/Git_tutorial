bc2cc5056f9e8dce65562baba2054ee7a987acb5
Angel Chavez <angelgabrielchavez18@gmail.com>
2021-10-14 22:48:56 -0600

    -setup.py/test- update

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     setup(setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     python_requires=">=3.6",
-    test_suite='basstatpl/tests',
+    test_suite='tests',
     packages=find_packages())
